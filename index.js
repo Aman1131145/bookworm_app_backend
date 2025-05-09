ed8a8135efc07106f15e51576998faab1837664d
Aman Mishra <mishraaman113114@gmail.com>
2025-05-09 21:59:24 +0530

    cloudinary issue resolved

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import job from './lib/cron.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import job from './lib/cron.js';
 
-import authRoutes from './routes/authRoutes.js'
-import bookRoutes from './routes/bookRoutes.js'
+import authRoutes from './routes/authRoutes.js';
+import bookRoutes from './routes/bookRoutes.js';
 
 import { connectDB } from './lib/db.js';
 
@@ -13,11 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 job.start();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`);
     connectDB();
-})
+});
